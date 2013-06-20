eed97b91e8508502ea24a3cfece4fb50186d875c
Ashwin Gaddam <gaddam_ashwin@yahoo.com>
2013-06-19 21:32:44 -0400

    Commit

---
+++ DownloadWcfService/ITransferService.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+namespace DownloadWcfService
+{
+    [ServiceContract]
+    public interface ITransferService
+    {
+        [OperationContract]
+        RemoteFileInfo DownloadFile(DownloadRequest request);
+
+        [OperationContract]
+        void UploadFile(RemoteFileInfo request);
+    }
+    [DataContract]
+    public class DownloadRequest
+    {
+        [DataMember]
+        public string FileName;
+    }
+
+    [DataContract]
+    public class RemoteFileInfo : IDisposable
+    {
+        [DataMember]
+        public string FileName;
+
+        [DataMember]
+        public long Length;
+
+        [DataMember]
+        public byte[] FileByteStream;
+
+        public void Dispose()
+        {
+            if (FileByteStream != null)
+            {
+                FileByteStream = null;
+            }
+        }
+    }
+}
--- MEF_Core_UI/MainPage.xaml.cs
+++ MEF_Core_UI/MainPage.xaml.cs
@@ -19,6 +19,10 @@ using System.Linq;
 using System.ComponentModel;
 using System.Reflection;
 using System.IO;
+using Windows.Storage;
+using Windows.Storage.Streams;
+using App1.ServiceReferenceTransfer;
+using System.Threading.Tasks;
 // The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=234238
 
 namespace App1
@@ -44,17 +48,49 @@ namespace App1
 
         private void Grid_Loaded_1(object sender, RoutedEventArgs e)
         {
+            ////var r = typeof(App).GetTypeInfo().Assembly;
+            //Assembly assembly = Assembly.Load(new AssemblyName("ClassLibrary1, Version=1.0.0.0, Culture=neutral, PublicKeyToken=null"));
+            //var configuration = new ContainerConfiguration().WithAssembly(assembly);
+
+            //using (var container = configuration.CreateContainer())
+            //{
+            //    var childView = container.GetExport<InterfaceLibrary.MainView>();
+            //    this.Frame.Navigate(childView.GetType(), null);
+            //}
+        }
+
+        private async void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            await savetofile("MainView.xaml");
+            await savetofile("ClassLibrary1.dll");
+
             var r = typeof(App).GetTypeInfo().Assembly;
-            //AssemblyName name = new AssemblyName("ClassLibrary1.dll");
+
             Assembly assembly = Assembly.Load(new AssemblyName("ClassLibrary1, Version=1.0.0.0, Culture=neutral, PublicKeyToken=null"));
             var configuration = new ContainerConfiguration().WithAssembly(assembly);
-            
 
             using (var container = configuration.CreateContainer())
             {
-                var greeter = container.GetExport<InterfaceLibrary.MainView>();
-                this.Frame.Navigate(greeter.GetType(), null);
+                var childView = container.GetExport<InterfaceLibrary.MainView>();
+                this.Frame.Navigate(childView.GetType(), null);
             }
+        }
+
+        private async Task savetofile(string fileName)
+        {
+            ServiceReferenceTransfer.TransferServiceClient svc = new ServiceReferenceTransfer.TransferServiceClient();
+            ServiceReferenceTransfer.DownloadRequest req = new ServiceReferenceTransfer.DownloadRequest();
+            req.FileName = fileName; 
+            var r1 = svc.DownloadFileAsync(req);
+            r1.Wait();
+            var file1 = r1.Result;
+            StorageFile sampleFile = await Windows.Storage.ApplicationData.Current.LocalFolder.CreateFileAsync(file1.FileName, CreationCollisionOption.ReplaceExisting);
+            IRandomAccessStream writeStream = await sampleFile.OpenAsync(FileAccessMode.ReadWrite);
+            IOutputStream outputSteam = writeStream.GetOutputStreamAt(0);
+            DataWriter dataWriter = new DataWriter(outputSteam);
+            dataWriter.WriteBytes(file1.FileByteStream);
+            await dataWriter.StoreAsync();
+            await outputSteam.FlushAsync();
         }
     }
 }
